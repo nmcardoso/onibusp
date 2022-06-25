f0b9ab20d86390284e357255c3fe9762c719262e
Natanael <natanael_magalhaes@msn.com>
2022-06-25 17:31:07 -0300

    remove import não usados

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import axios from 'axios'
 import 'leaflet/dist/leaflet.css'
 import dynamic from 'next/dynamic'
-import { useQuery } from 'react-query'
-import icon from 'leaflet/dist/images/marker-icon.png'
 
 const Map = dynamic(
   import('../components/Map'),
   { ssr: false }
 )
-
 
 export default function Home() {
   return (
