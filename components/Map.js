978b03eaa29894480eccdd1ed25a46e0ee210029
Natanael <natanael_magalhaes@msn.com>
2022-07-28 23:00:46 -0300

    componentes separados

--- components/Map.js
+++ components/Map.js
@@ -3,6 +3,9 @@ import 'leaflet/dist/leaflet.css'
 import { useQuery } from 'react-query'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import BusMarker from './BusMarker'
+import BusRoute from './BusRoute'
+import MapControllers from './MapControllers'
+import Modal from './Modal'
 
 const center = [-23.5595306, -46.7299148]
 
@@ -28,16 +31,21 @@ export default function Map() {
   if (status == 'error') return <h1>Error!<br />{error.message}</h1>
 
   return (
-    <MapContainer
-      center={center}
-      zoom={16}
-      style={{ width: '100vw', height: '100vh' }}
-    >
-      <TileLayer
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        attribution=''
-      />
-      {markers}
-    </MapContainer>
+    <>
+      <MapContainer
+        center={center}
+        zoom={16}
+        style={{ width: '100vw', height: '100vh' }}
+        doubleClickZoom={false}
+      >
+        <TileLayer
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution=''
+        />
+        {markers}
+        <BusRoute lineCode={2023} />
+        <MapControllers />
+      </MapContainer>
+    </>
   )
 }
