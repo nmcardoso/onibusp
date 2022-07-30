6b3ea3f695059d90968ec1a61266e7a8d7fe87c5
Natanael <natanael_magalhaes@msn.com>
2022-07-30 17:29:17 -0300

    adiciona  `BusRouteController`

--- components/MapControllers.js
+++ components/MapControllers.js
@@ -1,5 +1,6 @@
 import styles from '../styles/MapControllers.module.scss'
 import BusLineController from './BusLineController'
+import BusRouteController from './BusRouteController'
 import ConfigController from './ConfigController'
 
 export default function MapControllers() {
@@ -9,6 +10,7 @@ export default function MapControllers() {
         <div className={styles.container}>
           {/* <ConfigController /> */}
           <BusLineController />
+          <BusRouteController />
         </div>
       </div>
     </div>
