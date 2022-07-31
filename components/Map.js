d0ad085493c432c979cb37330120dc2ea722f78b
Natanael <natanael_magalhaes@msn.com>
2022-07-31 15:31:35 -0300

    usando componente ZoomControl declarativo

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,17 @@
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
 import BusMarkers from './BusMarkers'
 import BusRoutes from './BusRoutes'
 import MapControllers from './MapControllers'
+import { store } from '../utils/store'
+import { useContext } from 'react'
 
 const center = [-23.5595306, -46.7299148]
 
 export default function Map() {
+  const appContext = useContext(store)
+  const { appState } = appContext
+
   return (
     <>
       <MapContainer
@@ -16,11 +21,13 @@ export default function Map() {
         dragging={true}
         doubleClickZoom={false}
         scrollWheelZoom={true}
+        zoomControl={false}
       >
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution=''
         />
+        {appState.config.showZoomController ? <ZoomControl position="topleft" /> : null}
         <BusMarkers />
         <BusRoutes />
         <MapControllers />
