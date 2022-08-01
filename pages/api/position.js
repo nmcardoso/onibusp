626fd02ec367fc5b9b87a22e7174e5b2b85a049e
Natanael <natanael_magalhaes@msn.com>
2022-08-01 05:04:28 -0300

    atualização da api para obter a posição de todas as 10 linhas

--- pages/api/position.js
+++ pages/api/position.js
@@ -2,9 +2,13 @@
 import axios from 'axios'
 import { wrapper } from 'axios-cookiejar-support'
 import { CookieJar } from 'tough-cookie'
-import { BL_IDS } from '../../utils/constants'
 
-const jar = new CookieJar()
+// força os ônibus circulares a terem o mesmo código de linha mapeando
+// o código de um para outro
+const MASK = {
+  35313: 2524, // 8032-10
+  34144: 1376, // 7411-10
+}
 
 const posUrl = c => `Posicao/Linha?codigoLinha=${c}`
 const authUrl = `Login/Autenticar?token=${process.env.SPTRANS_TOKEN}`
@@ -12,56 +16,91 @@ const authUrl = `Login/Autenticar?token=${process.env.SPTRANS_TOKEN}`
 const client = wrapper(axios.create({
   baseURL: 'http://api.olhovivo.sptrans.com.br/v2.1/',
   timeout: 6000,
-  jar: jar
+  jar: new CookieJar()
 }))
 
-const MS_PER_MINUTE = 60000
+const AUTH_DURATION = 3600000 // 1 hour
 let lastAuth = null
 
 const auth = async () => {
   const now = new Date()
-  const maxAge = new Date(now - 60 * MS_PER_MINUTE)
 
-  if (!lastAuth || lastAuth < maxAge) {
+  if (!lastAuth || lastAuth < now - AUTH_DURATION) {
     await client.post(authUrl)
     lastAuth = now
   }
 }
 
-// força os ônibus circulares a terem o mesmo código de linha mapeando
-// o código de um para outro
-let maskedIds = [...BL_IDS]
-maskedIds[BL_IDS.indexOf(35313)] = 2545
-
-let lastResponse = null
-let cachedResponse = null
+const CACHE = [] // [{data: {...}, time: new Date()}]
+const CACHE_DURATION = 4500
 
 
-export default async function handler(req, res) {
+const batchFetch = async (lineIds) => {
+  const requests = lineIds.map(lineId => client.get(posUrl(lineId)))
+  const responses = await Promise.all(requests)
+  const data = responses.map(({ data }) => data)
+  const now = new Date()
+
+  const preparedData = data.map((v, i) => {
+    v['cl'] = lineIds[i] in MASK ? MASK[lineIds[i]] : lineIds[i]
+    return v
+  })
+
+  for (let i = 0; i < preparedData.length; i++) {
+    const idx = CACHE.findIndex(({ data }) => data.cl == preparedData[i].cl)
+    const dataToInsert = { data: preparedData[i], time: now }
+    if (idx !== -1) {
+      CACHE[idx] = dataToInsert
+    } else {
+      CACHE.push(dataToInsert)
+    }
+  }
+
+  return preparedData
+}
+
+
+const handlePostRequest = async (req, res) => {
   try {
     await auth()
 
     const now = new Date()
-    if (!lastResponse || lastResponse < now - 3200) {
-      const requests = BL_IDS.map(lineId => client.get(posUrl(lineId)))
-      const responses = await Promise.all(requests)
-      const data = responses.map(({ data }) => data)
 
-      const aggregate = data.map((v, i) => {
-        v['cl'] = maskedIds[i]
-        return v
-      })
+    const lineIds = req.body?.q || []
 
-      cachedResponse = aggregate
-      lastResponse = now
-      res.json(aggregate)
-    } else {
-      res.json(cachedResponse)
-    }
+    console.log(lineIds)
+
+    const cachedData = CACHE.filter(({ data, time }) => {
+      return lineIds.includes(data.cl) && !(time < now - CACHE_DURATION)
+    }).map(({ data }) => data)
+
+    const cachedIds = cachedData.map(({ cl }) => cl)
+
+    const idsToUpdate = lineIds.filter(cl => {
+      return !cachedIds.includes(cl)
+    })
+
+    const updatedData = await batchFetch(idsToUpdate)
+
+    const allData = [...cachedData, ...updatedData]
+
+    // console.log('cached', cachedData)
+    // console.log('updated', updatedData)
+
+    res.json(allData)
   } catch (e) {
     console.log(e.message)
     console.log(e.request?.res?.responseUrl)
     console.log(e.response?.data?.Message)
     res.send('error')
+  }
+}
+
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    await handlePostRequest(req, res)
+  } else {
+    res.send('Invalid method')
   }
 }
